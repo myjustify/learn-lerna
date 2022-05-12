fff7735def9f425de4e1c1f0443573d59c3c60ba
duanyong <1406023741@qq.com>
2022-05-12 16:11:08 +0800

    版本号

--- packages/y-generator/index.js
+++ packages/y-generator/index.js
@@ -10,7 +10,7 @@ import vue3DialogPrompt from './prompts/vue3DialogPrompt.js'
 const program = new Command()
 
 program
-  .version('1.0.172')
+  .version('1.0.173')
   .command('create')
   .description('create template')
   .action( async () => {
